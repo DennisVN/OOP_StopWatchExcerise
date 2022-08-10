1b10ab1e2a727cc36ac8059e8e05de137a02ef83
DennisVN <78357110+DennisVN@users.noreply.github.com>
2022-08-10 14:32:19 +0200

    Add files via upload
    add files

---
+++ UdemyStopwatch/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace UdemyStopwatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch clock = new Stopwatch();
+            // little menu to display
+           string instructions =  
+                @"RETE GRAVE STOPWATCH MENU :" + Environment.NewLine +
+                "1. Type 'start' to start the counter " + Environment.NewLine +
+                "2. Type 'stop' to stop the counter" + Environment.NewLine +
+                "3. PROFIT "+ Environment.NewLine +
+                "4. Type 'exit' to stop the counter" ;
+
+            Console.WriteLine(instructions);
+
+            while (true)
+            {
+
+                string input = Console.ReadLine().ToLower();
+
+                if (input == "start")
+                {
+                    // start Stopwatch
+                    Console.Clear();
+                    clock.StartWatch();
+                    Console.WriteLine("stopwatch started at " + DateTime.Now);
+                }
+                else if( input == "stop" )
+                {
+                    // stop Stopwatch
+                    clock.StopWatch();
+                    Console.WriteLine(instructions);
+                } else if ( input == "stop")
+                {
+                    Console.WriteLine("CIAO BRO");
+                    break;
+                }
+
+            } 
+
+
+
+        }
+    }
+}
---
+++ UdemyStopwatch/Stopwatch.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UdemyStopwatch
+{
+    public class Stopwatch
+    {
+        private DateTime _startCount;
+        private DateTime _stopCount;
+        private TimeSpan _totalTime;
+        private bool _isRunning;
+
+        public void StartWatch()
+        {   if (_isRunning)
+            {
+                throw new InvalidOperationException("Stopwatch is already running");
+            }
+            _startCount = DateTime.Now;
+            _isRunning = true;
+
+            Console.WriteLine("stopwatch started ");
+        }
+
+        public TimeSpan StopWatch()
+        {
+            if (_isRunning)
+            {
+                throw new InvalidOperationException("Stopwatch is not running");
+            }
+
+            _stopCount = DateTime.Now;
+            _isRunning = false;
+            _totalTime = _stopCount - _startCount;
+
+            Console.WriteLine("stopwatch stopped : " + _totalTime);
+
+            return _totalTime;
+        }
+    }
+}
+
+
